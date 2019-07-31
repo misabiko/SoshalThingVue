4bb5dbbd707cb4a991e463f3e040dbb34a50f2c8
misabiko <misabiko@gmail.com>
2019-07-31 03:19:50 -0400

    Commented toURI + moved twitter notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 //https://stackoverflow.com/a/57124645/2692695
+//Formats object into RESTful URI parameters (?param1=boop&param2=bap)
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function toURI(params : {[name: string] : any}) {
 	return '?' + Object.entries(params)
 		.map(
@@ -8,76 +10,6 @@ function toURI(params : {[name: string] : any}) {
 		)
 		.join("&");
 }
-
-/*
-contributors: null
-coordinates: null
-created_at: "Fri Jul 26 10:41:53 +0000 2019"
-entities: {hashtags: Array(0), symbols: Array(0), user_mentions: Array(0), urls: Array(0)}
-favorite_count: 0
-favorited: false
-geo: null
-id: 1154703380016615400
-id_str: "1154703380016615424"
-in_reply_to_screen_name: null
-in_reply_to_status_id: null
-in_reply_to_status_id_str: null
-in_reply_to_user_id: null
-in_reply_to_user_id_str: null
-is_quote_status: false
-lang: "ja"
-place: null
-retweet_count: 0
-retweeted: false
-source: "<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>"
-text: "花火の音が聞こえる"
-truncated: false
-user:
-	contributors_enabled: false
-	created_at: "Mon Apr 22 10:45:39 +0000 2013"
-	default_profile: false
-	default_profile_image: false
-	description: "のんびりだらだらお絵描きマン like:スプラトゥーン A&C/ポケモン/東方/ アイコン→【@sakibuizu2】"
-	entities: {url: {…}, description: {…}}
-	favourites_count: 3800
-	follow_request_sent: false
-	followers_count: 479
-	following: true
-	friends_count: 348
-	geo_enabled: false
-	has_extended_profile: true
-	id: 1371833978
-	id_str: "1371833978"
-	is_translation_enabled: false
-	is_translator: false
-	lang: null
-	listed_count: 29
-	location: "やけたとう"
-	name: "かまち＠絆"
-	notifications: false
-	profile_background_color: "9AE4E8"
-	profile_background_image_url: "http://abs.twimg.com/images/themes/theme16/bg.gif"
-	profile_background_image_url_https: "https://abs.twimg.com/images/themes/theme16/bg.gif"
-	profile_background_tile: false
-	profile_banner_url: "https://pbs.twimg.com/profile_banners/1371833978/1555606290"
-	profile_image_url: "http://pbs.twimg.com/profile_images/1001402893927268352/K4FU-liF_normal.jpg"
-	profile_image_url_https: "https://pbs.twimg.com/profile_images/1001402893927268352/K4FU-liF_normal.jpg"
-	profile_link_color: "0084B4"
-	profile_sidebar_border_color: "BDDCAD"
-	profile_sidebar_fill_color: "DDFFCC"
-	profile_text_color: "333333"
-	profile_use_background_image: true
-	protected: false
-	screen_name: "kamati0maru"
-	statuses_count: 31373
-	time_zone: null
-	translator_type: "none"
-	url: "https://t.co/QmgE8EaHm5"
-	utc_offset: null
-	verified: false
-	__proto__: Object
-	__proto__: Object
-*/
 
 let statuses = [] as any[];
 let homePromise : Promise<void> = fetch('http://localhost:43043/statuses/home_timeline')
@@ -157,3 +89,73 @@ class Timeline extends React.Component<{}, TimelineState> {
 }
 
 export default Timeline;
+
+/*
+contributors: null
+coordinates: null
+created_at: "Fri Jul 26 10:41:53 +0000 2019"
+entities: {hashtags: Array(0), symbols: Array(0), user_mentions: Array(0), urls: Array(0)}
+favorite_count: 0
+favorited: false
+geo: null
+id: 1154703380016615400
+id_str: "1154703380016615424"
+in_reply_to_screen_name: null
+in_reply_to_status_id: null
+in_reply_to_status_id_str: null
+in_reply_to_user_id: null
+in_reply_to_user_id_str: null
+is_quote_status: false
+lang: "ja"
+place: null
+retweet_count: 0
+retweeted: false
+source: "<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>"
+text: "花火の音が聞こえる"
+truncated: false
+user:
+	contributors_enabled: false
+	created_at: "Mon Apr 22 10:45:39 +0000 2013"
+	default_profile: false
+	default_profile_image: false
+	description: "のんびりだらだらお絵描きマン like:スプラトゥーン A&C/ポケモン/東方/ アイコン→【@sakibuizu2】"
+	entities: {url: {…}, description: {…}}
+	favourites_count: 3800
+	follow_request_sent: false
+	followers_count: 479
+	following: true
+	friends_count: 348
+	geo_enabled: false
+	has_extended_profile: true
+	id: 1371833978
+	id_str: "1371833978"
+	is_translation_enabled: false
+	is_translator: false
+	lang: null
+	listed_count: 29
+	location: "やけたとう"
+	name: "かまち＠絆"
+	notifications: false
+	profile_background_color: "9AE4E8"
+	profile_background_image_url: "http://abs.twimg.com/images/themes/theme16/bg.gif"
+	profile_background_image_url_https: "https://abs.twimg.com/images/themes/theme16/bg.gif"
+	profile_background_tile: false
+	profile_banner_url: "https://pbs.twimg.com/profile_banners/1371833978/1555606290"
+	profile_image_url: "http://pbs.twimg.com/profile_images/1001402893927268352/K4FU-liF_normal.jpg"
+	profile_image_url_https: "https://pbs.twimg.com/profile_images/1001402893927268352/K4FU-liF_normal.jpg"
+	profile_link_color: "0084B4"
+	profile_sidebar_border_color: "BDDCAD"
+	profile_sidebar_fill_color: "DDFFCC"
+	profile_text_color: "333333"
+	profile_use_background_image: true
+	protected: false
+	screen_name: "kamati0maru"
+	statuses_count: 31373
+	time_zone: null
+	translator_type: "none"
+	url: "https://t.co/QmgE8EaHm5"
+	utc_offset: null
+	verified: false
+	__proto__: Object
+	__proto__: Object
+*/
