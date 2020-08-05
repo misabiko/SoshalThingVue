7688136d4a7060df17f73cdcef2b782b773625ff
misabiko <misabiko@gmail.com>
2020-08-05 19:22:34 -0400

    Formatting

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@
 const visible = (function() {
 	let stateKey : string, eventKey : string;
 	const keys : { [name : string] : string } = {
-		hidden: "visibilitychange",
-		webkitHidden: "webkitvisibilitychange",
-		mozHidden: "mozvisibilitychange",
-		msHidden: "msvisibilitychange"
+		hidden: 'visibilitychange',
+		webkitHidden: 'webkitvisibilitychange',
+		mozHidden: 'mozvisibilitychange',
+		msHidden: 'msvisibilitychange'
 	};
 	for (stateKey in keys) {
 		if (stateKey in document) {
@@ -27,7 +27,7 @@ function toURI(params : { [name : string] : any }) {
 		.map(
 			([key, val]) => `${encodeURIComponent(key)}=${encodeURIComponent(val)}`
 		)
-		.join("&");
+		.join('&');
 }
 
 function getPostObject(postData : PostData) : Post {
@@ -38,18 +38,18 @@ class PostMedia {
 	element : HTMLDivElement;
 
 	constructor(public imageSrcs : string[]) {
-		this.element = document.createElement("div");
-		this.element.className = "soshalPMedia soshalPMedia" + this.imageSrcs.length;
+		this.element = document.createElement('div');
+		this.element.className = 'soshalPMedia soshalPMedia' + this.imageSrcs.length;
 
 
 		for (let i = 0; i < this.imageSrcs.length; i++) {
-			const mediaHolder = document.createElement("div");
-			mediaHolder.className = "soshalMediaHolder";
+			const mediaHolder = document.createElement('div');
+			mediaHolder.className = 'soshalMediaHolder';
 			this.element.append(mediaHolder);
 
-			const img = document.createElement("img");
-			img.addEventListener("load", PostMedia.handleImageLoaded);
-			img.alt = "img" + i;
+			const img = document.createElement('img');
+			img.addEventListener('load', PostMedia.handleImageLoaded);
+			img.alt = 'img' + i;
 			img.src = this.imageSrcs[i];
 			mediaHolder.append(img);
 		}
@@ -58,7 +58,7 @@ class PostMedia {
 	static handleImageLoaded(loadEvent : Event) {
 		const img = loadEvent.target as HTMLImageElement;
 		if (img.parentElement)
-			img.parentElement.classList.add(img.width > img.height ? "landscape" : "portrait");
+			img.parentElement.classList.add(img.width > img.height ? 'landscape' : 'portrait');
 	}
 }
 
@@ -82,24 +82,24 @@ class Post {
 	commentButton : HTMLButtonElement;
 
 	constructor(public data : PostData) {
-		this.element = document.createElement("div");
-		this.element.className = "soshalPost";
-		this.element.setAttribute("post-id", data.id);
+		this.element = document.createElement('div');
+		this.element.className = 'soshalPost';
+		this.element.setAttribute('post-id', data.id);
 
-		const sideDiv = document.createElement("div");
-		sideDiv.className = "soshalPSide";
+		const sideDiv = document.createElement('div');
+		sideDiv.className = 'soshalPSide';
 		this.element.append(sideDiv);
 
-		const avatar = document.createElement("img");
-		avatar.alt = this.data.authorHandle + "'s avatar";
+		const avatar = document.createElement('img');
+		avatar.alt = this.data.authorHandle + '\'s avatar';
 		avatar.src = this.data.authorAvatar;
 		sideDiv.append(avatar);
 
-		const span = document.createElement("span");
-		span.append(this.data.authorName, "@" + this.data.authorHandle);
+		const span = document.createElement('span');
+		span.append(this.data.authorName, '@' + this.data.authorHandle);
 		this.element.append(span);
 
-		const p = document.createElement("p");
+		const p = document.createElement('p');
 		p.textContent = this.data.text;
 		this.element.append(p);
 
@@ -108,8 +108,8 @@ class Post {
 			this.element.append(this.postMedia.element);
 		}
 
-		const buttons = document.createElement("div");
-		buttons.className = "soshalPButtons";
+		const buttons = document.createElement('div');
+		buttons.className = 'soshalPButtons';
 		buttons.append(
 			this.createRepostButton(),
 			this.createLikeButton(),
@@ -120,7 +120,7 @@ class Post {
 	static createButton(iconName : string, iconStyle : string, className : string, onClick : () => Promise<void>) {
 		const button = document.createElement('button');
 		button.className = className;
-		button.addEventListener("click", onClick);
+		button.addEventListener('click', onClick);
 
 		const icon = document.createElement('i');
 		icon.className = iconStyle + ' fa-' + iconName;
@@ -131,7 +131,7 @@ class Post {
 
 	createLikeButton() {
 		this.likeButton = Post.createButton('heart', this.data.liked ? 'fas' : 'far', 'likeButton', async () => {
-			const twitResp = await fetch('twitter/like/' + this.data.id, {method: "POST"})
+			const twitResp = await fetch('twitter/like/' + this.data.id, {method: 'POST'})
 				.then(response => response.json());
 			this.setLiked(twitResp.favorited);
 		});
@@ -154,7 +154,7 @@ class Post {
 
 	createRepostButton() {
 		this.repostButton = Post.createButton('retweet', 'fas', 'repostButton', async () => {
-			const twitResp = await fetch('twitter/retweet/' + this.data.id, {method: "POST"})
+			const twitResp = await fetch('twitter/retweet/' + this.data.id, {method: 'POST'})
 				.then(response => response.json());
 			if (twitResp.retweeted)
 				this.repostButton.classList.add('repostedPostButton');
@@ -182,9 +182,9 @@ class Repost extends Post {
 	constructor(data : RepostData) {
 		super(data);
 
-		const repostDiv = document.createElement("div");
-		repostDiv.append(data.reposterName + " retweeted");
-		repostDiv.className = "soshalRepostLabel";
+		const repostDiv = document.createElement('div');
+		repostDiv.append(data.reposterName + ' retweeted');
+		repostDiv.className = 'soshalRepostLabel';
 		this.element.prepend(repostDiv);
 	}
 }
@@ -197,17 +197,17 @@ class Timeline {
 	postContainer : HTMLDivElement;
 
 	constructor(readonly name : string, readonly endpoint : string, private options : any = {}, private refreshRate : number = 90000) {
-		this.element = document.createElement("div");
-		this.element.className = "soshalTimeline";
+		this.element = document.createElement('div');
+		this.element.className = 'soshalTimeline';
 
-		const header = document.createElement("div");
-		header.className = "soshalTHeader";
-		header.addEventListener("click", () => this.refresh());
+		const header = document.createElement('div');
+		header.className = 'soshalTHeader';
+		header.addEventListener('click', () => this.refresh());
 		header.textContent = this.name;
 		this.element.append(header);
 
-		this.postContainer = document.createElement("div");
-		this.postContainer.className = "soshalTPosts";
+		this.postContainer = document.createElement('div');
+		this.postContainer.className = 'soshalTPosts';
 		this.element.append(this.postContainer);
 
 		//Init refreshing and set it as event callback on visible/focus
@@ -238,11 +238,11 @@ class Timeline {
 	}
 
 	async refresh() {
-		const newPostDatas = await fetch('/twitter/tweets/' + this.endpoint + (this.options ? toURI(this.options) : ""))
+		const newPostDatas = await fetch('/twitter/tweets/' + this.endpoint + (this.options ? toURI(this.options) : ''))
 			.then(response => response.json())
 			.then(newData => {
 				console.log(newData.length);
-				console.log(typeof(newData));
+				console.log(typeof (newData));
 				return newData.reverse().filter((a : PostData) => this.posts.findIndex(b => b.data.id === a.id) < 0);
 			});
 
@@ -265,7 +265,7 @@ class Sidebar {
 	element : HTMLDivElement;
 
 	constructor() {
-		this.element = document.createElement("div");
+		this.element = document.createElement('div');
 		this.element.id = 'soshalSidebar';
 	}
 }
@@ -275,22 +275,23 @@ class LoginBar {
 	loginLink : HTMLAnchorElement;
 
 	constructor() {
-		this.element = document.createElement("div");
+		this.element = document.createElement('div');
 		this.element.id = 'soshalLoginBar';
 
-		this.loginLink = document.createElement("a");
+		this.loginLink = document.createElement('a');
 		this.element.append(this.loginLink);
 
-		this.setMessage("");
+		this.setMessage('');
 	}
 
-	setMessage(message: string) {
+	setMessage(message : string) {
 		if (message) {
-			this.loginLink.text = message;
-			this.element.classList.remove("loginBarHidden");
+			this.loginLink.href = message;
+			this.loginLink.text = 'Twitter Login';
+			this.element.classList.remove('loginBarHidden');
 		}else {
-			this.loginLink.href = "";
-			this.element.classList.add("loginBarHidden");
+			this.loginLink.href = '';
+			this.element.classList.add('loginBarHidden');
 		}
 	}
 }
@@ -304,11 +305,11 @@ class SoshalThing {
 	loggedIn = false;
 
 	constructor() {
-		this.element = document.createElement("div");
-		this.element.id = "soshalThing";
+		this.element = document.createElement('div');
+		this.element.id = 'soshalThing';
 
-		this.timelineContainer = document.createElement("div");
-		this.timelineContainer.id = "soshalTimelineContainer";
+		this.timelineContainer = document.createElement('div');
+		this.timelineContainer.id = 'soshalTimelineContainer';
 		this.element.append(this.timelineContainer);
 
 		this.element.append(this.sidebar.element);
@@ -320,9 +321,8 @@ class SoshalThing {
 			.then(json => {
 				if (json === 'LoggedIn')
 					this.setLoggedIn(true);
-				else {
+				else
 					this.loginBar.setMessage(json.auth_url);
-				}
 			});
 	}
 
@@ -337,16 +337,16 @@ class SoshalThing {
 			timeline.resetRefreshing();
 
 		if (loggedIn)
-			this.timelineContainer.classList.remove("squishedTimelineContainer");
+			this.timelineContainer.classList.remove('squishedTimelineContainer');
 		else
-			this.timelineContainer.classList.add("squishedTimelineContainer");
+			this.timelineContainer.classList.add('squishedTimelineContainer');
 	}
 }
 
 const soshalThing = new SoshalThing();
-soshalThing.addTimeline(new Timeline("Home", "home_timeline"));
-soshalThing.addTimeline(new Timeline("Art", "search", {"q": "list:misabiko/Art filter:media -filter:retweets"}, 10000));
-soshalThing.addTimeline(new Timeline("Mentions", "search", {"q": "misabiko -from:misabiko -from:GoldenMisabiko"}, 10000));
-soshalThing.addTimeline(new Timeline("#深夜の真剣お絵描き60分一本勝負", "search", {"q": "#深夜の真剣お絵描き60分一本勝負 filter:media -filter:retweets"}, 10000));
+soshalThing.addTimeline(new Timeline('Home', 'home_timeline'));
+soshalThing.addTimeline(new Timeline('Art', 'search', {'q': 'list:misabiko/Art filter:media -filter:retweets'}, 10000));
+soshalThing.addTimeline(new Timeline('Mentions', 'search', {'q': 'misabiko -from:misabiko -from:GoldenMisabiko'}, 10000));
+soshalThing.addTimeline(new Timeline('#深夜の真剣お絵描き60分一本勝負', 'search', {'q': '#深夜の真剣お絵描き60分一本勝負 filter:media -filter:retweets'}, 10000));
 
 window.onload = () => document.body.append(soshalThing.element);
