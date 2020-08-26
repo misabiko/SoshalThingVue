0557c0b70c4c31b08c2297f016edf57804811ef9
misabiko <misabiko@gmail.com>
2020-08-26 09:57:46 -0400

    Added a small script to update tweet samples

---
+++ scripts/updateTweetSamples.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+const got = require('got')
+const {CookieJar} = require('tough-cookie')
+
+;(async () => {
+	const sampleTweetsPath = path.join(__dirname, '../cypress/fixtures/sampleTweets')
+	const cookieJar = new CookieJar()
+
+	await got('http://localhost:3000/twitter/access', {cookieJar})
+
+	for (const tweet of fs.readdirSync(sampleTweetsPath)) {
+		const tweetPath = path.join(sampleTweetsPath, tweet)
+		const {id} = JSON.parse(fs.readFileSync(tweetPath))
+		console.log(`Updating tweet ${id} at ${tweet}`)
+
+		const {post} = await got('http://localhost:3000/twitter/tweets/status/' + id, {
+			responseType: 'json',
+			resolveBodyOnly: true,
+			cookieJar,
+		})
+
+		fs.writeFileSync(tweetPath, JSON.stringify(post, null, '\t'))
+	}
+
+	console.log('\nDone.')
+})()
