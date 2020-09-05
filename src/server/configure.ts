04c57fbf0900202626782cf53808e093716c0de5
misabiko <misabiko@gmail.com>
2020-09-05 09:28:25 -0400

    Implemented a production ready (?) memory store

--- src/server/configure.ts
+++ src/server/configure.ts
@@ -3,15 +3,22 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import session from 'express-session';
+import connectMemoryStore from 'memorystore';
 import passport from 'passport';
 import {Common} from './routes/common';
 import {Twitter} from './routes/twitter';
 
 export default (app : Express) => {
+	const MemoryStore = connectMemoryStore(session);
+
 	app.use(morgan('dev'));
 	app.use(cookieParser());
 	app.use(bodyParser.json());
 	app.use(session({
+		cookie: {maxAge: 86400000},
+		store: new MemoryStore({
+			checkPeriod: 86400000,
+		}),
 		secret: 'ehyAv3bH2AwKMMWiOgOeNlOYg',
 		resave: false,
 		saveUninitialized: true,
