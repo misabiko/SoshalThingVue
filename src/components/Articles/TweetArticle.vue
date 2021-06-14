69198533f3818934133236600dc68ce9b3042dc5
misabiko <misabiko@gmail.com>
2021-06-13 20:15:36 -0400

    Parsing retweets

--- src/components/Articles/TweetArticle.vue
+++ src/components/Articles/TweetArticle.vue
@@ -1,10 +1,10 @@
 <template>
-	<article class='article' :articleId='article.id'>
+	<article class='article' :articleId='actualArticle.id'>
 <!--		superheader-->
 		<div class='media'>
 			<figure class='media-left'>
 				<p class='image is-64x64'>
-					<img :src='article.authorAvatar' :alt="article.authorHandle + '\'s avatar'">
+					<img :src='actualArticle.author.avatarURL' :alt="actualArticle.author.handle + '\'s avatar'">
 				</p>
 			</figure>
 			<div class='media-content'>
@@ -12,40 +12,40 @@
 					<div class='articleHeader'>
 						<a
 							class='names'
-							:href='service.getUserURL(article.authorHandle)'
+							:href='service.getUserURL(actualArticle.author.handle)'
 							target='_blank'
 							rel='noopener noreferrer'
-							@click.left.prevent='addUserTimeline(article.authorHandle)'
+							@click.left.prevent='addUserTimeline(actualArticle.author.handle)'
 						>
-							<strong>{{ article.authorName }}</strong>
-							<small>@{{article.authorHandle}}</small>
+							<strong>{{ actualArticle.author.name }}</strong>
+							<small>@{{actualArticle.author.handle}}</small>
 						</a>
 						<span class='timestamp'>
 							<small title='long time!'>short time!</small>
 						</span>
 					</div>
-					<div class='tweet-paragraph'>{{article.content}}</div>
+					<div v-if='actualArticle.type !== TwitterArticleType.Retweet' class='tweet-paragraph'>{{ actualArticle.text }}</div>
 				</div>
 <!--			extra-->
 				<nav class='level is-mobile'>
 					<div class='level-left'>
 						<a
 							class='level-item articleButton repostButton'
-							:class='{repostedPostButton: article.reposted}'
+							:class='{repostedPostButton: actualArticle.reposted}'
 						>
 						<span class='icon'>
 							<FontAwesomeIcon icon='retweet'/>
 						</span>
-							<span v-if='article.repostCount'>{{article.repostCount}}</span>
+							<span v-if='actualArticle.repostCount'>{{actualArticle.repostCount}}</span>
 						</a>
 						<a
 							class='level-item articleButton likeButton'
-							:class='{likedPostButton: article.liked}'
+							:class='{likedPostButton: actualArticle.liked}'
 						>
 						<span class='icon'>
-							<FontAwesomeIcon :icon='[article.liked ? "fas" : "far", "heart"]'/>
+							<FontAwesomeIcon :icon='[actualArticle.liked ? "fas" : "far", "heart"]'/>
 						</span>
-							<span v-if='article.likeCount'>{{article.likeCount}}</span>
+							<span v-if='actualArticle.likeCount'>{{actualArticle.likeCount}}</span>
 						</a>
 						<a class='level-item articleButton articleMenuButton'>
 						<span class='icon'>
@@ -61,9 +61,9 @@
 </template>
 
 <script lang='ts'>
-import {defineComponent, inject, PropType} from 'vue'
+import {computed, defineComponent, inject, PropType, toRefs} from 'vue'
 import {Service} from '@/services'
-import {TwitterArticle, TwitterService} from '@/services/Twitter'
+import {TwitterArticle, TwitterService, TwitterArticleType, RetweetArticle, QuoteArticle} from '@/services/Twitter'
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {faEllipsisH, faHeart as fasHeart, faRetweet} from '@fortawesome/free-solid-svg-icons'
 import {faHeart as farHeart} from '@fortawesome/free-regular-svg-icons'
@@ -87,15 +87,24 @@ export default defineComponent({
 	},
 
 	setup(props) {
-		const service = inject<Service>('service')
-		if (!service)
-			throw "Article wasn't provided a service"
+		const {article, service} = toRefs(props)
+
+		const actualArticle = computed<TwitterArticle>(() => {
+			switch (article.value.type) {
+				case TwitterArticleType.Tweet:
+					return article.value
+				case TwitterArticleType.Retweet:
+					return service.value.articles[(article.value as RetweetArticle).retweetedId]
+				case TwitterArticleType.Quote:
+					return service.value.articles[(article.value as QuoteArticle).quotedId]
+			}
+		})
 
 		function addUserTimeline(handle : string) {
 			console.log('boop ' + handle)
 		}
 
-		return {service, addUserTimeline}
+		return {service, addUserTimeline, TwitterArticleType, actualArticle}
 	}
 })
 </script>
--- src/services/Twitter.ts
+++ src/services/Twitter.ts
@@ -1,24 +1,52 @@
-import {Endpoint, Payload, Service, WrappedPayload} from '@/services/index'
+import {Endpoint, Payload, Service} from '@/services/index'
 import {Article, MediaLoadStatus} from '@/data/articles'
-import TweetArticle from '@/components/Articles/TweetArticle.vue'
+import TweetComponent from '@/components/Articles/TweetArticle.vue'
 import {TimelineData} from '@/data/timelines'
+import TweetArticle from '@/components/Articles/TweetArticle.vue'
+
+export enum TwitterArticleType {
+	Tweet,
+	Retweet,
+	Quote,
+}
+
+export interface TwitterUser {
+	id : string
+	name : string
+	handle : string
+	avatarURL : string
+}
 
 export interface TwitterArticle extends Article {
-	content : string
-	authorName : string
-	authorHandle : string
-	authorAvatar : string
+	type : TwitterArticleType
+	creationDate : Date
+	author : TwitterUser
+}
+
+export interface TweetArticle extends TwitterArticle {
+	type : TwitterArticleType.Tweet | TwitterArticleType.Quote
+	text : string
 	liked : boolean
 	reposted : boolean
 	likeCount : number
 	repostCount : number
 }
 
+export interface RetweetArticle extends TwitterArticle {
+	type : TwitterArticleType.Retweet
+	retweetedId : string
+}
+
+export interface QuoteArticle extends TweetArticle {
+	type : TwitterArticleType.Quote
+	quotedId : string
+}
+
 export class TwitterService extends Service {
 	articles! : { [id : string] : TwitterArticle }
 
 	constructor() {
-		super('Twitter', TweetArticle)
+		super('Twitter', TweetComponent)
 
 		this.endpoints.push(new UserTimelineEndpoint())
 	}
@@ -40,12 +68,54 @@ export class TwitterService extends Service {
 	}
 
 	getExternalLink(id : string) : string {
-		return ''
+		const {id: tweetId, author} = this.articles[id]
+		return `https://twitter.com/${author.handle}/status/${tweetId}`
 	}
 
 	getUserURL(handle : string) {
 		return 'https://twitter.com/' + handle
 	}
+}
+
+interface TwitterAPIPayload {
+	data : APIPayloadTweetData[]
+	includes : {
+		users: APIUserData[]
+		tweets: APITweetData[]
+	}
+	meta : {
+		oldest_id: string
+		newest_id: string
+		result_count: number
+		next_token: string
+	}
+}
+
+interface APIUserData {
+	username: string
+	id: string
+	profile_image_url: string
+	name: string
+}
+
+interface APITweetData {
+	id : string
+	created_at: string
+	text : string
+	author_id : string
+	public_metrics: {
+		retweet_count: number
+		reply_count: number
+		like_count: number
+		quote_count: number
+	}
+}
+
+interface APIPayloadTweetData extends APITweetData {
+	referenced_tweets: {
+		type: "retweeted"
+		id : string
+	}[]
 }
 
 interface UserTimelineInstanceOpt {
@@ -64,39 +134,100 @@ class UserTimelineEndpoint extends Endpoint<UserTimelineInstanceOpt, UserTimelin
 	async call(options : UserTimelineCallOpt) : Promise<Payload> {
 		const payload : Payload<TwitterArticle> = {articles: [], newArticles: []}
 		const params = new URLSearchParams()
-		params.set('tweet.fields', 'created_at,public_metrics,entities')
-		params.set('expansions', 'author_id')
+		params.set('tweet.fields', 'created_at,public_metrics,entities,referenced_tweets')
+		params.set('expansions', 'author_id,referenced_tweets.id,referenced_tweets.id.author_id')
 		params.set('user.fields', 'name,username,profile_image_url')
 
-		const response = await fetch(`/twitter/users/${options.userId}?${params.toString()}`).then(r => r.json())
+		const response : TwitterAPIPayload = await fetch(`/twitter/users/${options.userId}?${params.toString()}`).then(r => r.json())
 		console.dir(response)
 
-		for (const tweet of response.data) {
-			const user = response.includes.users.find((u : any) => u.id === tweet.author_id)
+		dataTweetLoop: for (const tweet of response.data) {
+			const user = response.includes.users.find((u : APIUserData) => u.id === tweet.author_id)
+			if (!user) {
+				console.error(`Couldn't find user ${tweet.author_id} for article ${tweet.id}`)
+				continue
+			}
 
-			payload.articles.push({
-				id: tweet.id,
-				content: tweet.text,
-				index: 0,
-				media: {
-					status: MediaLoadStatus.NothingLoaded,
-				},
-				liked: false,
-				likeCount: tweet.public_metrics.like_count,
-				reposted: false,
-				repostCount: tweet.public_metrics.retweet_count,
-				authorAvatar: user.profile_image_url,
-				authorHandle: user.username,
-				authorName: user.name,
-				hidden: false,
-				queried: false,
-			})
-			payload.newArticles.push(tweet.id)
+			for (const referencedTweet of tweet.referenced_tweets)
+				switch (referencedTweet.type) {
+					case 'retweeted':
+						const retweetedTweet = response.includes.tweets.find((t : APITweetData) => t.id === referencedTweet.id)
+						if (!retweetedTweet) {
+							console.error(`Couldn't find retweeted article ${referencedTweet.id} for retweet ${tweet.id}`)
+							continue dataTweetLoop
+						}
+
+						const retweetedTweetAuthor = response.includes.users.find((u : APIUserData) => u.id === retweetedTweet.author_id)
+						if (!retweetedTweetAuthor) {
+							console.error(`Couldn't find retweeted article author ${retweetedTweet.author_id} for retweet ${tweet.id}`)
+							continue dataTweetLoop
+						}
+
+						const {articles, newArticles} = this.parseRetweet(tweet, user, retweetedTweet, retweetedTweetAuthor)
+						payload.articles.push(...articles)
+						payload.newArticles.push(...newArticles)
+						break;
+					default:
+						console.warn(`Reference tweet type "${referencedTweet.type}"`)
+				}
 		}
 
 		this.updateInstance(options, payload)
 
 		return payload
+	}
+
+	parseRetweet(retweet : APIPayloadTweetData, author : APIUserData, retweetedTweet : APITweetData, retweetedAuthor : APIUserData) {
+		const result : Payload<TwitterArticle> = {articles: [], newArticles: []}
+
+		result.articles.push(<TweetArticle>{
+			type: TwitterArticleType.Tweet,
+			id: retweetedTweet.id,
+			creationDate: new Date(retweetedTweet.created_at),
+			text: retweetedTweet.text,
+			author: {
+				id: retweetedAuthor.id,
+				handle: retweetedAuthor.username,
+				name: retweetedAuthor.name,
+				avatarURL: retweetedAuthor.profile_image_url,
+			},
+			index: 0,
+			media: {
+				status: MediaLoadStatus.NothingLoaded,
+			},
+			liked: false,
+			likeCount: retweetedTweet.public_metrics.like_count,
+			reposted: false,
+			repostCount: retweetedTweet.public_metrics.retweet_count,
+			hidden: false,
+			queried: false,
+		})
+
+		result.articles.push(<RetweetArticle>{
+			type: TwitterArticleType.Retweet,
+			id: retweet.id,
+			creationDate: new Date(retweet.created_at),
+			retweetedId: retweetedTweet.id,
+			author: {
+				id: author.id,
+				handle: author.username,
+				name: author.name,
+				avatarURL: author.profile_image_url,
+			},
+			index: 0,
+			media: {
+				status: MediaLoadStatus.NothingLoaded,
+			},
+			liked: false,
+			likeCount: retweet.public_metrics.like_count,
+			reposted: false,
+			repostCount: retweet.public_metrics.retweet_count,
+			hidden: false,
+			queried: false,
+		})
+		result.newArticles.push(retweet.id)
+
+		return result
 	}
 
 	updateInstance(options : UserTimelineCallOpt, payload : Payload) {
@@ -158,5 +289,4 @@ class HomeTimelineEndpoint extends Endpoint<HomeTimelineInstanceOpt, HomeTimelin
 			articles: [],
 		}
 	}
-
 }
