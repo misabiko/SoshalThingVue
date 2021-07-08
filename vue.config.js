8b15e4939ebbaa5902a3b8984ddb907b152e189c
misabiko <misabiko@gmail.com>
2021-07-07 22:56:49 -0400

    Pulled old html options from Vue2

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 const configureAPI = require('./src/server/configure')
 
 module.exports = {
+	pages: {
+		index: {
+			entry: 'src/main.ts',
+			title: 'SoshalThing',
+		},
+	},
 	devServer: {
 		before: configureAPI,
 		disableHostCheck: true
