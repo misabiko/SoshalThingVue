250833a3b2ade139c9a209ae2f8a93f2b3192f35
misabiko <misabiko@gmail.com>
2021-07-12 21:17:07 -0400

    Fixed style not parsing in mocha

--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,14 @@ module.exports = {
 	devServer: {
 		before: configureAPI,
 		disableHostCheck: true
-	}
+	},
+	chainWebpack: (config) => {
+		if (process.env.NODE_ENV === 'test') {
+			const sassRule = config.module.rule('sass');
+			sassRule.uses.clear();
+			sassRule
+				.use('null-loader')
+				.loader('null-loader');
+		}
+	},
 }
