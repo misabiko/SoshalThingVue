7be77e2aee0eb97ddbcaf7fa55709bc2ff205e30
misabiko <misabiko@gmail.com>
2021-07-13 02:54:11 -0400

    cypress is building!

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,7 +3,7 @@
 describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App')
+    cy.get('#timelineContainer').children().should('have.length', 0)
   })
 })
 
